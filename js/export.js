172e397d0424753115bd99c405f568cce901a70b
ReyLan <orangpolos860@gmail.com>
2025-12-31 22:17:07 +0700

    Jadwal Sholat by Reyzar

--- js/export.js
+++ js/export.js
@@ -18,6 +18,23 @@ function drawWatermark(ctx, width, height) {
   };
 }
 
+if (!CanvasRenderingContext2D.prototype.roundRect) {
+  CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
+    this.beginPath();
+    this.moveTo(x + r, y);
+    this.lineTo(x + w - r, y);
+    this.quadraticCurveTo(x + w, y, x + w, y + r);
+    this.lineTo(x + w, y + h - r);
+    this.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
+    this.lineTo(x + r, y + h);
+    this.quadraticCurveTo(x, y + h, x, y + h - r);
+    this.lineTo(x, y + r);
+    this.quadraticCurveTo(x, y, x + r, y);
+    this.closePath();
+    return this;
+  };
+}
+
 function getExportFileName(ext) {
   const monthNames = [
     "Januari","Februari","Maret","April","Mei","Juni",
